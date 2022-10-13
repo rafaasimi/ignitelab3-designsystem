4b7efefbf48a9ece660d06478e95dd8e7ca196fa
Rafael Simionato <rafaasimi@hotmail.com>
2022-10-13 13:47:42 -0300

    Adicionado arquivo de configuração CI Storybook Workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, {configType}) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/ignitelab3-designsystem/'
+    }
+
+    return config
   }
 }
